38958a66dbb3853436699b0fa0232329d1afcc3e
Angele <giraudae@hotmail.fr>
2014-12-09 17:38:35 +0100

    Suppression dans interface_manager de gestion mot de passe du manager

--- src/Interface_manager.java
+++ src/Interface_manager.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 
+import java.awt.Dimension;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.event.ChangeEvent;
@@ -193,39 +194,6 @@ public class Interface_manager extends JFrame {
     }
 
 
-    public class JPasswordField2 implements ActionListener {
-
-        JPasswordField passwordField1 = null;
-
-        public void Manager () {
-            JPasswordField2 jpf2 = new JPasswordField2();
-            jpf2.init();
-        }
-
-        public void init() {
-            JFrame f = new JFrame("ma fenetre");
-            f.setSize(300, 100);
-            JPanel pannel = new JPanel();
-
-            passwordField1 = new JPasswordField("");
-            passwordField1.setPreferredSize(new Dimension(100, 20));
-            pannel.add(passwordField1);
-
-            JButton bouton1 = new JButton("Afficher");
-            bouton1.addActionListener(this);
-
-            pannel.add(bouton1);
-            f.getContentPane().add(pannel);
-            f.setVisible(true);
-        }
-
-        public void actionPerformed(ActionEvent e) {
-            int comparaison = String.copyValueOf(passwordField1.getPassword()).compareToIgnoreCase(String.valueOf(motDePasse));
-            if(comparaison == 0) {	// Si le mot de passe est le bon
-
-            }
-            // System.out.println("texte saisie = " + String.copyValueOf(passwordField1.getPassword()));
-        }
 
 
 
