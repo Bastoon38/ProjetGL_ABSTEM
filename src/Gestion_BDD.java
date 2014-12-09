ed1d5593818d8269dd6296f85465fbe4eace9c74
Maxime <arcanis223@gmail.com>
2014-12-09 14:14:20 +0100

    Fichier Gestion BDD

--- src/Gestion_BDD.java
+++ src/Gestion_BDD.java
@@ -1,36 +1,48 @@
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.sql.*;
 
 public class Gestion_BDD {
 
 	public void verifVitrine(String nom_prod) {
-		
+
 	}
-	
+
 	public void supprimerStock(String nom, int quantite) {
 		String url = "localhost";
 		String login = "root";
 		String password = "root";
 		try{
-			
+
 			//Class.forName("com.mysql.jdbc.Driver");
-			
+
 			Connection con = DriverManager.getConnection(url,login,password);
-			
+
 			Statement stmt = con.createStatement();
-			ResultSet rs = stmt.executeQuery("DELETE * FROM Stocks WHERE ");
+			ResultSet rs = stmt.executeQuery("SELECT `QUANTITE` FROM Stocks WHERE `PRODUIT`='" + nom + "'");
+
+			if (rs.getInt("QUANTITE") > quantite) {
+				rs.updateInt("QUANTITE",rs.getInt("QUANTITE")-quantite);
+			}
+			else if (rs.getInt("QUANTITE") == quantite) {
+				rs.deleteRow();
+			}
+			if (rs.getInt("QUANTITE") < quantite) {
+
+				rs.updateInt("QUANTITE",rs.getInt("QUANTITE")-quantite);
+			}
+			con.commit();
+
+			rs.close();
+			stmt.close();
+			con.close();
+
 		}
 		catch(SQLException sqle){
 			System.out.print(sqle.getMessage());
 		}
 	}
-	
+
 	public void ajouterStock(String nom, int quantite) {
-		
+
 	}
 }
