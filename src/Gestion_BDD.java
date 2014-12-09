1393c938e60f57046a7ab8bc65c099f29f4be37a
arca223 <arcanis223@gmail.com>
2014-12-09 17:32:31 +0100

    Librairie mysql connector + supprStock
    Fonction suppr stock finie pour le cas ou le stock est suppérieur à la
    quantité voulant être supprimée

--- src/Gestion_BDD.java
+++ src/Gestion_BDD.java
@@ -3,42 +3,69 @@ import java.sql.*;
 
 public class Gestion_BDD {
 
-	public void verifVitrine(String nom_prod) {
+	public Gestion_BDD(){
 
 	}
 
-	public void supprimerStock(String nom, int quantite) {
-		String url = "localhost";
+	//verifie la péremption des produits en vitrine
+	public void verifVitrine(String nom_prod) {
+			System.out.println("test");
+	}
+
+	//mise en cuisson du produit, suppression du stock
+	public String supprimerStock(String nom, int quantite) {
+		String url = "jdbc:mysql://localhost/albm_dev";
 		String login = "root";
 		String password = "root";
+		String ret = "Suppression du stock finie";
+
 		try{
 
 			//Class.forName("com.mysql.jdbc.Driver");
 
 			Connection con = DriverManager.getConnection(url,login,password);
 			Statement stmt = con.createStatement();
-			ResultSet rs = stmt.executeQuery("SELECT `QUANTITE` FROM Stocks WHERE `PRODUIT`='" + nom + "'");
+			ResultSet rs = stmt.executeQuery("SELECT `QUANTITE` FROM Stock WHERE `PRODUIT`='" + nom + "'");
 
-			if (rs.getInt("QUANTITE") > quantite) {
-				rs.updateInt("QUANTITE",rs.getInt("QUANTITE")-quantite);
+
+			rs.next();
+			int qteStock = rs.getInt("QUANTITE");
+
+			if (qteStock > quantite) {
+
+				int val = qteStock - quantite;
+				Statement stmt2 = con.createStatement();
+				int res = stmt.executeUpdate("UPDATE `Stock` SET `QUANTITE`=" + String.valueOf(val) +" WHERE `PRODUIT`='" + nom + "'");
 			}
-			else if (rs.getInt("QUANTITE") == quantite) {
+			else if (qteStock == quantite) {
 				rs.deleteRow();
 			}
-			if (rs.getInt("QUANTITE") < quantite) {
+			if (qteStock < quantite) {
 
-				rs.updateInt("QUANTITE",rs.getInt("QUANTITE")-quantite);
+				int qte1 = quantite-qteStock; //on récupère la différence, en positif
+				rs.deleteRow();
+				if (rs.next()) {
+					int val = rs.getInt("QUANTITE") - qte1;
+					Statement stmt2 = con.createStatement();
+					int res = stmt.executeUpdate("UPDATE `Stock` SET `QUANTITE`=" + String.valueOf(val) +" WHERE `PRODUIT`='" + nom + "'");
+				} else {
+					ret = "Il manque " + qte1 + " " + nom + " au stock";
+				}
+
 			}
-			con.commit();
 
 			rs.close();
 			stmt.close();
 			con.close();
 
+
+
 		}
 		catch(SQLException sqle){
-			System.out.print(sqle.getMessage());
+			return sqle.getMessage();
 		}
+
+		return ret;
 	}
 
 	public void ajouterStock(String nom, int quantite) {
