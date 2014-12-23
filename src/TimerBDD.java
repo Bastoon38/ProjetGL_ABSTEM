b961487f9b0157b114507a532c0cc3605640d876
Angele <giraudae@hotmail.fr>
2014-12-23 14:33:33 +0100

    Restructuration Timer

--- src/TimerBDD.java
+++ src/TimerBDD.java
@@ -3,56 +3,60 @@
  */
 import java.util.*;
 import java.util.TimerTask;
-//import java.util.*;
 
-/*
-public class TimerBDD {
 
-    public static void main (String lieu, int annee, int mois, int jour, int heure, int minutes) {
+public class TimerBDD {/*
+    private String lieu = new String();
+    private int annee, mois, jour, heure, minutes;
 
-        if ((mois == 0) && (jour == 0)) {
-            chrono (heure, minutes);
-        }
-        else {
-            peremption (lieu, annee, mois, jour, heure, minutes);
-        }
+    public TimerBDD (String lieu1, int annee1, int mois1, int jour1, int heure1, int minutes1) {
+        lieu = lieu1;
+        annee = annee1;
+        mois = mois1;
+        jour = jour1;
+        heure = heure1;
+        minutes = minutes1;
     }
 
-    public static boolean chrono (int heure, int minutes) {
-        TimerBDD timer1 = new TimerBDD();   // crée timer
-        //TimerTask task = new TimerTask();
-        timer1.schedule(new TimerTask() {
-            public void run()
-            {
-                System.out.printf ("Tâche chrono lancée\n");
+    public void main (String args[]) {
+
+        if ((annee == 0) && (mois == 0) && (jour == 0)) {   // Durée à chronométrer
+            TimerBDD timer1 = new TimerBDD();   // crée timer
+            //TimerTask task = new TimerTask();
+            timer1.schedule(new TimerTask() {
+            }, (long)0, (long)((heure/60)+minutes)/60000);
+            // 0 est le délai avant de commencer, le deuxième est la durée en millisecondes
+            //return true;    // Produit devenu périmé
+        }
+
+        else {  // Date de péremption à vérifier
+           /* Date dateFin = new Date(annee, mois, jour, heure, minutes);
+            Timer timer = new Timer();// crée timer
+
+            timer.schedule(new TimerTask() {
+                public void run()
+                {
+                    System.out.printf ("Tâche peremption lancée\n");
+                }
+            }, dateFin);
+
+            System.out.printf ("Tâche peremption finie\n");
+            // Produit devenu périmé
+            if (lieu == "Stock"){
+                GestionBDD.supprimerPerime ("Stock");
             }
-        }, (long)0, (long)((heure/60)+minutes)/60000);
-        // 0 est le délai avant de commencer, le deuxième est la durée en millisecondes
-
-        return true;    // Produit devenu périmé
-    }
-
-    public static void peremption (String lieu, int annee, int mois, int jour, int heure, int minutes) {
-        //@SuppressWarnings("deprecation")
-        Date dateFin = new Date(annee, mois, jour, heure, minutes);
-        Timer timer = new Timer();// crée timer
-
-        timer.schedule(new TimerTask() {
-            public void run()
-            {
-                 System.out.printf ("Tâche peremption lancée\n");
+            if (lieu == "Vitrine"){
+                GestionBDD.supprimerPerime ("Vitrine");
             }
-        }, dateFin);
-
-        System.out.printf ("Tâche peremption finie\n");
-
-        // Produit devenu périmé
-        if (lieu == "Stock"){
-            GestionBDD.supprimerPerime ("Stock");
-        }
-
-        if (lieu == "Vitrine"){
-            GestionBDD.supprimerPerime ("Vitrine");
         }
     }
-} */
+
+
+
+    public void run()
+    {
+        System.out.printf ("Tâche chrono lancée\n");
+    }
+
+*/
+}
