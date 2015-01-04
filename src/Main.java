8aad99ed20f45c9dc6e0796378718bd9c67fdc92
Angele <giraudae@hotmail.fr>
2015-01-04 18:26:15 +0100

    Main normal

--- src/Main.java
+++ src/Main.java
@@ -14,16 +14,16 @@ public class Main {
         //Interface_vendeur frame = new Interface_vendeur();
         //frame.setVisible(true);
 
-        GestionBDD bdd = new GestionBDD();
-        System.out.println(bdd.getTime_cuisson("Baguette",14));
+        //GestionBDD bdd = new GestionBDD();
+        //System.out.println(bdd.getTime_cuisson("Baguette",14));
         //Elio Test Vendeur
         // ihm_billet billet = new ihm_billet();
         //billet.setVisible(true);
         //Interface_vendeur frame = new Interface_vendeur();
         //Vendeur vendeur= new Vendeur(frame);
 
-        //Connexion frame1 = new Connexion();
-        //frame1.setVisible(true);
+        Connexion frame1 = new Connexion();
+        frame1.setVisible(true);
 
         //Timer timer = new Timer();
         //timer.schedule(new TimerMajCuisson(), 0, 150000);
